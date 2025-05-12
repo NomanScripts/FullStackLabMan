f2727d8bf7a5201e70a5998a0393b01cf7e3ded5
NomanScripts <noman@busona.co>
2025-05-12 16:12:10 +0500

    404 server error fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Define backend URLs
-const LOCAL_API_URL = 'http://localhost:5000'
-const PROD_API_URL = 'http://16.16.77.239:5000'
-
-// Determine which URL to use based on environment
-const target =
-  process.env.NODE_ENV === 'production' ? PROD_API_URL : LOCAL_API_URL
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy API requests to backend
-      '/api': {
-        target,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
